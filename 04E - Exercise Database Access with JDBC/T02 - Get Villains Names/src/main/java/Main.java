ccc8d1d6e4ec3bbfe1c792307815eb5e1176b316
z1n0v1 <z1n0v1@students.softuni.bg>
2022-01-24 05:28:23 +0200

    Added T02 - Get Villains Names from 04E - Exercise Database Access with JDBC. Changed the directory structure

---
+++ 04E - Exercise Database Access with JDBC/T02 - Get Villains Names/src/main/java/Main.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException, SQLException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        Connection connection = connectToDB(reader);
+
+        Statement statement = connection.createStatement();
+        statement.execute(
+                "SELECT v.name, count(DISTINCT mv.minion_id) as 'count' " +
+                        "    FROM villains AS v " +
+                        "    RIGHT JOIN minions_villains mv on v.id = mv.villain_id " +
+                        "    GROUP BY v.name " +
+                        "    HAVING `count` > 15" +
+                        "    ORDER BY `count` DESC"
+        );
+        ResultSet set = statement.getResultSet();
+
+        while (set.next()) {
+            System.out.printf("%s %d", set.getString("name"), set.getInt("count"));
+        }
+    }
+
+    private static Connection connectToDB(BufferedReader reader) throws IOException, SQLException {
+
+        System.out.print("DB User (default:root): ");
+        String user = reader.readLine();
+        user = user.equals("") ? "root" : user;
+
+        System.out.print("DB Password: ");
+        String password = reader.readLine();
+
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/minions_db", user, password);
+    }
+}
