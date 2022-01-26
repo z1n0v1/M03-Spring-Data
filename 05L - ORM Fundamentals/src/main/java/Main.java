5999f320015f09faf9afe0b1574e2d45b399842d
z1n0v1 <z1n0v1@students.softuni.bg>
2022-01-26 11:56:24 +0200

    05L - ORM Fundamentals - an ugly but working solution

---
+++ 05L - ORM Fundamentals/src/main/java/Main.java
@@ -0,0 +1,31 @@
+import entities.User;
+import orm.EntityManager;
+import orm.MyConnector;
+
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) throws SQLException, IllegalAccessException,
+            InvocationTargetException, NoSuchMethodException, InstantiationException {
+        MyConnector.createConnection("your_username","your_password","users_test");
+        Connection connection = MyConnector.getConnection();
+
+        EntityManager<User> entityManager = new EntityManager<>(connection);
+
+//        User user = new User("gosho","poznaime", 13,
+//                LocalDate.of(2022, 1, 1));
+//        entityManager.persist(user);
+
+        User found = entityManager.findFirst(User.class, "username = 'gosho'");
+
+        System.out.println(found);
+
+        for (User user1 : entityManager.find(User.class, "username = 'gosho'")) {
+            System.out.println(user1);
+        }
+
+    }
+}
---
+++ 05L - ORM Fundamentals/src/main/java/entities/User.java
@@ -0,0 +1,74 @@
+package entities;
+
+import orm.anotations.Column;
+import orm.anotations.Entity;
+import orm.anotations.Id;
+
+import java.time.LocalDate;
+
+@Entity(name = "users")
+public class User {
+    @Id
+    private long id;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "age")
+    private int age;
+    @Column(name = "registration")
+    private LocalDate registration;
+
+    public User(String username, String password, int age, LocalDate registration) {
+        this.username = username;
+        this.password = password;
+        this.age = age;
+        this.registration = registration;
+    }
+
+    public User() {
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", age=" + age +
+                ", registration=" + registration +
+                '}';
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public LocalDate getRegistration() {
+        return registration;
+    }
+
+    public void setRegistration(LocalDate registration) {
+        this.registration = registration;
+    }
+}
