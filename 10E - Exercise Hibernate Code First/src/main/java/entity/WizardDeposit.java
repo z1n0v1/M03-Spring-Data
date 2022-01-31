bf8f207836b82eb9a56da88f579cd15f881ae672
z1n0v1 <z1n0v1@students.softuni.bg>
2022-01-31 08:30:55 +0200

    T01 - Gringotts Database - 10E - Exercise Hibernate Code First

---
+++ 10E - Exercise Hibernate Code First/src/main/java/entity/WizardDeposit.java
@@ -0,0 +1,149 @@
+package entity;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "wizard_deposits")
+public class WizardDeposit extends BaseEntity {
+
+    private String firstName;
+    private String lastName;
+    private String notes;
+    private Integer age;
+    private String magicWandCreator;
+    private Short magicWandSize;
+    private String depositGroup;
+    private LocalDateTime depositStartDate;
+    // depositAmount, depositInterest and depositCharge could be BigDecimal
+    private Double depositAmount;
+    private Double depositInterest;
+    private Double depositCharge;
+    private LocalDateTime depositExpirationDate;
+    private boolean isDepositExpired;
+
+    public WizardDeposit() {
+    }
+
+    @Column(name = "first_name", length = 50)
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Column(name = "last_name", length = 60, nullable = false)
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Column(name = "notes", columnDefinition = "TEXT")
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    @Column(name = "age", nullable = false)
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Column(name = "magic_wand_creator", length = 100)
+    public String getMagicWandCreator() {
+        return magicWandCreator;
+    }
+
+    public void setMagicWandCreator(String magicWandCreator) {
+        this.magicWandCreator = magicWandCreator;
+    }
+
+    @Column(name = "magic_wand_size")
+    public Short getMagicWandSize() {
+        return magicWandSize;
+    }
+
+
+    public void setMagicWandSize(Short magicWandSize) {
+        this.magicWandSize = magicWandSize;
+    }
+
+    @Column(name = "deposit_group", length = 20)
+    public String getDepositGroup() {
+        return depositGroup;
+    }
+
+    public void setDepositGroup(String depositGroup) {
+        this.depositGroup = depositGroup;
+    }
+
+    @Column(name = "deposit_start_date")
+//    @Temporal(TemporalType.TIMESTAMP)
+    public LocalDateTime getDepositStartDate() {
+        return depositStartDate;
+    }
+
+    public void setDepositStartDate(LocalDateTime depositStartDate) {
+        this.depositStartDate = depositStartDate;
+    }
+
+    @Column(name = "deposit_amount")
+    public Double getDepositAmount() {
+        return depositAmount;
+    }
+
+    public void setDepositAmount(Double depositAmount) {
+        this.depositAmount = depositAmount;
+    }
+
+    @Column(name = "deposit_interest")
+    public Double getDepositInterest() {
+        return depositInterest;
+    }
+
+    public void setDepositInterest(Double depositInterest) {
+        this.depositInterest = depositInterest;
+    }
+
+    @Column(name = "deposit_charge")
+    public Double getDepositCharge() {
+        return depositCharge;
+    }
+
+    public void setDepositCharge(Double depositCharge) {
+        this.depositCharge = depositCharge;
+    }
+
+    @Column(name = "deposit_expiration_date")
+//    @Temporal(TemporalType.TIMESTAMP)
+    public LocalDateTime getDepositExpirationDate() {
+        return depositExpirationDate;
+    }
+
+    public void setDepositExpirationDate(LocalDateTime depositExpirationDate) {
+        this.depositExpirationDate = depositExpirationDate;
+    }
+
+    @Column(name = "is_deposit_expired")
+    public boolean isDepositExpired() {
+        return isDepositExpired;
+    }
+
+    public void setDepositExpired(boolean depositExpired) {
+        isDepositExpired = depositExpired;
+    }
+}
